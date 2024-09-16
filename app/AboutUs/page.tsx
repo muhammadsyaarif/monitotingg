c8f6d0c46e3ea5ef05d43605e26bd2bd53e7fe00
muhammadsyaarif <148513751+muhammadsyaarif@users.noreply.github.com>
2024-09-16 16:44:31 +0700

    Update page.tsx

--- app/AboutUs/page.tsx
+++ app/AboutUs/page.tsx
@@ -38,10 +38,10 @@ export default function AboutUs() {
         Kembali ke Homepage
       </Link>
           <Link
-              href="/AboutUs"
+              href="/monitoring"
               className="relative inline-block px-4 sm:px-4 py-2 sm:py-2 text-lg sm:text-xl font-bold text-white bg-gradient-to-r from-yellow-400 to-orange-500 rounded-full shadow-lg hover:scale-105 hover:bg-gradient-to-r hover:from-orange-500 hover:to-yellow-400 transition duration-300 ease-in-out"
             >
-              About Us
+              Monitoring
             </Link>
     </div>
   );
